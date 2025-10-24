5be42f45ee052dd7d9e2538ae84e858f844ebceb
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-24 10:11:38 +0200

    feat: ajoute les utilitaires JWT, les classes de gestion de token

---
+++ src/main/java/fr/jessee/chatop/util/JsonWebToken.java
@@ -0,0 +1,55 @@
+package fr.jessee.chatop.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class JsonWebToken {
+    private static final String SECRET_KEY_STRING = "";
+    private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes(StandardCharsets.UTF_8));
+
+    public static String generateToken(String username, long tokenExpiration) {
+        Instant now = Instant.now();
+        return Jwts.builder()
+                .claim("sub", username)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plus(tokenExpiration, ChronoUnit.MILLIS)))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public static boolean isSignatureValid(String token) {
+        try {
+            Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static Claims getClaimsFromToken(String token) {
+        return Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
+    }
+
+    public static boolean isTokenValid(String token) {
+        try {
+            Claims claims = getClaimsFromToken(token);
+
+            return isSignatureValid(token) &&
+                    !isTokenExpired(claims);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isTokenExpired(Claims claims) {
+        Date expiration = claims.getExpiration();
+        return expiration.before(new Date());
+    }
+}
