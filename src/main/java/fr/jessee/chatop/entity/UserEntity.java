db1ecb3d07d24ddbcd70490fbab6d1fb46a94d74
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-24 11:45:03 +0200

    feat: implémente les méthodes CRUD dans UserService pour la gestion des utilisateurs

--- src/main/java/fr/jessee/chatop/entity/UserEntity.java
+++ src/main/java/fr/jessee/chatop/entity/UserEntity.java
@@ -1,23 +1,20 @@
 package fr.jessee.chatop.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 @Data
+@Entity
 public class UserEntity {
-    @Id
+    @jakarta.persistence.Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String email;
-
     private String name;
-
     private String password;
-
-    private String created_at;
-
-    private String updated_at;
+    private String createdAt;
+    private String updatedAt;
 }
