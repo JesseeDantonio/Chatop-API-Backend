4944d6af5959e488424da1cb0beb8ae4a787c023
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-24 09:17:57 +0200

    first commit

---
+++ src/test/java/fr/jessee/chatop/ChaTopApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jessee.chatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaTopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
