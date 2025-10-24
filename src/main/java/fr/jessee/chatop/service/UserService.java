f332ac553d13057205d32acb7a7e044c052bf55e
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-24 11:44:53 +0200

    feat: implémente les méthodes CRUD dans UserService pour la gestion des utilisateurs

--- src/main/java/fr/jessee/chatop/service/UserService.java
+++ src/main/java/fr/jessee/chatop/service/UserService.java
@@ -1,4 +1,46 @@
 package fr.jessee.chatop.service;
 
+import fr.jessee.chatop.entity.UserEntity;
+import fr.jessee.chatop.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public UserEntity getUserById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public UserEntity createUser(UserEntity user) {
+        return userRepository.save(user);
+    }
+
+    public UserEntity updateUser(Integer id, UserEntity user) {
+        UserEntity existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            existingUser.setEmail(user.getEmail());
+            existingUser.setName(user.getName());
+            existingUser.setPassword(user.getPassword());
+            existingUser.setCreatedAt(user.getCreatedAt());
+            existingUser.setUpdatedAt(user.getUpdatedAt());
+            return userRepository.save(existingUser);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
 }
