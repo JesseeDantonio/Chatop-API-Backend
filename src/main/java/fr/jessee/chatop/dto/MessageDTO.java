6d1d5af6abf8f7bbee6e42e621bfe02a1dd94a4d
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-26 14:50:17 +0100

    feat: ajoute le DTO MessageDTO

---
+++ src/main/java/fr/jessee/chatop/dto/MessageDTO.java
@@ -0,0 +1,9 @@
+package fr.jessee.chatop.dto;
+
+public class MessageDTO {
+    private Integer id;
+    private String content;
+    private Integer senderId;
+    private Integer receiverId;
+    private String timestamp;
+}
