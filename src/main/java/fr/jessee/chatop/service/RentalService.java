e57682bcc6d57dd833b49503c3232d1fb63bafe4
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-28 13:43:35 +0100

    refactor: utiliser RentalDTO et RentalCreateDTO dans RentalService et ajouter les mappings toDTO/toEntity

--- src/main/java/fr/jessee/chatop/service/RentalService.java
+++ src/main/java/fr/jessee/chatop/service/RentalService.java
@@ -1,37 +1,51 @@
 package fr.jessee.chatop.service;
 
+import fr.jessee.chatop.dto.in.RentalCreateDTO;
+import fr.jessee.chatop.dto.out.RentalDTO;
 import fr.jessee.chatop.entity.RentalEntity;
+import fr.jessee.chatop.entity.UserEntity;
 import fr.jessee.chatop.repository.RentalRepository;
+import fr.jessee.chatop.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class RentalService {
     private final RentalRepository rentalRepository;
+    private final UserRepository userRepository;
 
-    public RentalService(RentalRepository rentalRepository) {
+    public RentalService(RentalRepository rentalRepository,  UserRepository userRepository) {
         this.rentalRepository = rentalRepository;
+        this.userRepository = userRepository;
     }
 
-    public RentalEntity getById(int id) {
-        return rentalRepository.findById(id).orElse(null);
+    public RentalDTO getById(int id) {
+        RentalEntity rental = rentalRepository.findById(id).orElse(null);
+        if (rental != null) {
+            return toDTO(rental);
+        }
+        return null;
     }
 
-    public List<RentalEntity> getAllRentals() {
-        return rentalRepository.findAll();
+    public List<RentalDTO> getAllRentals() {
+        List<RentalDTO> rentals = new ArrayList<>();
+        rentalRepository.findAll().forEach(rental -> {
+            RentalDTO dto = toDTO(rental);
+            rentals.add(dto);
+        });
+        return rentals;
+
     }
 
-    public RentalEntity getOwnerById(Integer id) {
-        return rentalRepository.findById(id).orElse(null);
+    public RentalCreateDTO createRental(RentalCreateDTO rental) {
+        rentalRepository.save(toEntity(rental));
+        return rental;
     }
 
-    public RentalEntity createRental(RentalEntity message) {
-        return rentalRepository.save(message);
-    }
-
-    public RentalEntity updateRental(Integer id, RentalEntity rental) {
+    public RentalDTO updateRental(Integer id, RentalDTO rental) {
         RentalEntity existingRental = rentalRepository.findById(id).orElse(null);
         if (existingRental != null) {
             existingRental.setName(rental.getName());
@@ -39,8 +53,12 @@ public class RentalService {
             existingRental.setPrice(rental.getPrice());
             existingRental.setPicture(rental.getPicture());
             existingRental.setSurface(rental.getSurface());
+            UserEntity owner = userRepository.findById(rental.getOwnerId())
+                    .orElseThrow(() -> new RuntimeException("Owner not found"));
+            existingRental.setOwnerId(owner);
             existingRental.setUpdatedAt(LocalDate.now().toString());
-            return rentalRepository.save(existingRental);
+            rentalRepository.save(existingRental);
+            return toDTO(existingRental);
         } else {
             return null;
         }
@@ -48,5 +66,30 @@ public class RentalService {
 
     public void deleteRental(Integer id) {
         rentalRepository.deleteById(id);
+    }
+
+    private RentalDTO toDTO(RentalEntity entity) {
+        RentalDTO dto = new RentalDTO();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        dto.setDescription(entity.getDescription());
+        dto.setPrice(entity.getPrice());
+        dto.setSurface(entity.getSurface());
+        dto.setOwnerId(entity.getOwnerId().getId());
+        dto.setPicture(entity.getPicture());
+        return dto;
+    }
+
+    public RentalEntity toEntity(RentalCreateDTO dto) {
+        RentalEntity entity = new RentalEntity();
+        entity.setName(dto.getName());
+        entity.setSurface(dto.getSurface());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setPicture(dto.getPicture());
+        UserEntity owner = userRepository.findById(dto.getOwnerId())
+                .orElseThrow(() -> new RuntimeException("Owner not found"));
+        entity.setOwnerId(owner);
+        return entity;
     }
 }
