a85083c0daf68ec10561c176248c29fcc3e9c906
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-28 13:46:08 +0100

    feat: ajoute le DTO MessageCreateDTO pour représenter les données d'entrée d'un message

---
+++ src/main/java/fr/jessee/chatop/dto/in/MessageCreateDTO.java
@@ -0,0 +1,8 @@
+package fr.jessee.chatop.dto.in;
+
+public class MessageCreateDTO {
+    private String content;
+    private Integer senderId;
+    private Integer receiverId;
+    private String timestamp;
+}
