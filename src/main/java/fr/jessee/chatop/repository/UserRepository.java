04935bd232cb124b228a59e2d1187e3cabd773f2
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-31 14:00:12 +0100

    feat: ajouter méthode findById dans UserRepository pour récupérer un utilisateur par ID

--- src/main/java/fr/jessee/chatop/repository/UserRepository.java
+++ src/main/java/fr/jessee/chatop/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package fr.jessee.chatop.repository;
 
 import fr.jessee.chatop.dto.in.UserCreateDTO;
+import fr.jessee.chatop.dto.out.UserDTO;
 import fr.jessee.chatop.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserCreateDTO> findByEmail(String email);
+    Optional<UserEntity> findById(Long id);
 }
