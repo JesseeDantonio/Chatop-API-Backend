43c911af6e6a9a838c3ca981cf89aa7ed06ed89f
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-31 13:59:31 +0100

    feat: ajouter RefreshTokenEntity pour gérer les tokens de rafraîchissement dans l'authentification

---
+++ src/main/java/fr/jessee/chatop/entity/RefreshTokenEntity.java
@@ -0,0 +1,18 @@
+package fr.jessee.chatop.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@Entity
+public class RefreshTokenEntity {
+    @Id
+    private Long id;
+    private String token;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+    private Instant expirationDate;
+}
