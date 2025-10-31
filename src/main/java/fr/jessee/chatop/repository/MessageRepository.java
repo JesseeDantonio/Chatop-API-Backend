2e8e78ea2c314e04a7182da839e137fa989a5571
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-31 09:34:50 +0100

    refactor: modifier MessageRepository pour utiliser des requêtes JPQL avec @Query et @Param

--- src/main/java/fr/jessee/chatop/repository/MessageRepository.java
+++ src/main/java/fr/jessee/chatop/repository/MessageRepository.java
@@ -2,9 +2,15 @@ package fr.jessee.chatop.repository;
 
 import fr.jessee.chatop.entity.MessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<MessageEntity, Integer> {
-    MessageEntity findByRentalId(int rentalId);
 
-    MessageEntity findByUserId(int userId);
+    @Query("SELECT m FROM MessageEntity m WHERE m.userId = :userId")
+    List<MessageEntity> findByUserId(@Param("userId") int userId);
+    @Query("SELECT m FROM MessageEntity m WHERE m.rentalId = :rentalId")
+    List<MessageEntity> findByRentalId(@Param("rentalId") int rentalId);
 }
