e3998b25eebdf5e594e3708114837433b8109ae2
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-31 14:21:25 +0100

    refactor: modifier la méthode findByEmail dans UserRepository pour retourner Optional<UserEntity> au lieu de Optional<UserCreateDTO>

--- src/main/java/fr/jessee/chatop/repository/UserRepository.java
+++ src/main/java/fr/jessee/chatop/repository/UserRepository.java
@@ -1,13 +1,11 @@
 package fr.jessee.chatop.repository;
 
-import fr.jessee.chatop.dto.in.UserCreateDTO;
-import fr.jessee.chatop.dto.out.UserDTO;
 import fr.jessee.chatop.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-    Optional<UserCreateDTO> findByEmail(String email);
+    Optional<UserEntity> findByEmail(String email);
     Optional<UserEntity> findById(Long id);
 }
