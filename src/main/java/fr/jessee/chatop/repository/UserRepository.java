99fae69680c4667355e8f6fb1b040381abd81205
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-31 13:03:45 +0100

    feat: ajouter méthode findByEmail dans UserRepository pour récupérer un utilisateur par email

--- src/main/java/fr/jessee/chatop/repository/UserRepository.java
+++ src/main/java/fr/jessee/chatop/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package fr.jessee.chatop.repository;
 
+import fr.jessee.chatop.dto.in.UserCreateDTO;
 import fr.jessee.chatop.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserCreateDTO> findByEmail(String email);
 }
