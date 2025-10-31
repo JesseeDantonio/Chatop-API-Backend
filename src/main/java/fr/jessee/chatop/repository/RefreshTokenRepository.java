1c84e6b78d4e019c5329f264fd4aa29a91e8bddc
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-31 13:59:43 +0100

    feat: ajouter RefreshTokenRepository pour gérer les opérations sur les tokens de rafraîchissement

---
+++ src/main/java/fr/jessee/chatop/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package fr.jessee.chatop.repository;
+
+import fr.jessee.chatop.entity.RefreshTokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Integer> {
+    Optional<RefreshTokenEntity> findByToken(String token);
+    int deleteByUserId(Long userId);
+}
