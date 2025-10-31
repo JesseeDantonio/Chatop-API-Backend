ea8666490668c6d27413febdb01c27e5e027fd3d
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-10-31 10:01:01 +0100

    refactor: remplacer int par Long pour l'identifiant dans RentalEntity

--- src/main/java/fr/jessee/chatop/entity/RentalEntity.java
+++ src/main/java/fr/jessee/chatop/entity/RentalEntity.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class RentalEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String name;
 
