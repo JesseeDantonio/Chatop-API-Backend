9be49305b1588eef5d745dd58c08cce42c406e28
JesseeDantonio <dyland.antonio1998@gmail.com>
2025-11-16 15:52:54 +0100

    feat: ajouter les annotations @JsonProperty pour mapper created_at et updated_at dans RentalDTO

--- src/main/java/fr/jessee/chatop/dto/out/RentalDTO.java
+++ src/main/java/fr/jessee/chatop/dto/out/RentalDTO.java
@@ -1,5 +1,6 @@
 package fr.jessee.chatop.dto.out;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
@@ -11,6 +12,8 @@ public class RentalDTO {
     private String picture;
     private String description;
     private Long ownerId;
+    @JsonProperty("created_at")
     private String createdAt;
+    @JsonProperty("updated_at")
     private String updatedAt;
 }
