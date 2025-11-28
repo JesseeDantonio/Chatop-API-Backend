6f13dc41c361f094887fc0bab7ad8e41534b236b
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2025-11-28 12:20:31 +0100

    refactor: supprimer les méthodes inutilisées dans RefreshTokenRepository

--- src/main/java/fr/jessee/chatop/repository/RefreshTokenRepository.java
+++ src/main/java/fr/jessee/chatop/repository/RefreshTokenRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Integer> {
-    Optional<RefreshTokenEntity> findByToken(String token);
-    int deleteByUserId(Long userId);
 }
